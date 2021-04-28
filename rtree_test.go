06d7787fe9670bad10d571194b79df4f6b963d1e
Morten Raeburn <mortenraeburn@outlook.com>
2021-04-28 22:22:13 +0200

    Test half-spaces and count area
    Co-authored-by: Morten Raeburn <MortenRaeburn@users.noreply.github.com>

--- rtree_test.go
+++ rtree_test.go
@@ -17,6 +17,131 @@ func testEndLabel() {
 	fmt.Println()
 }
 
+func TestHalfspaceCountTwoNegative(t *testing.T) {
+	testStartLabel(t)
+	defer testEndLabel()
+
+	assert := assert.New(t)
+
+	data := [][2]float64{
+		{-3, 1},
+		{3, -4},
+		{5, -6},
+		{5, -5},
+		{5, -8},
+		{3, -6},
+		{5, -6},
+		{1, -2},
+		{5, -622},
+	}
+
+	tree, _ := NewRTree(data, 3, sumOfSlice, one)
+
+	l := new(line)
+
+	l.B = 0
+	l.M = 1
+
+	VO := tree.AuthCountHalfSpace(l, 0)
+
+	valid := verifyHalfSpace(len(data), l, VO, tree.Digest, 0, tree.Fanout)
+
+	assert.False(valid, "Should be true")
+
+}
+
+func TestHalfspaceCountTwo(t *testing.T) {
+	testStartLabel(t)
+	defer testEndLabel()
+
+	assert := assert.New(t)
+
+	data := [][2]float64{
+		{-3, 1},
+		{-1, 2},
+		{3, -4},
+		{5, -6},
+	}
+
+	tree, _ := NewRTree(data, 3, sumOfSlice, one)
+
+	l := new(line)
+
+	l.B = 0
+	l.M = 1
+
+	VO := tree.AuthCountHalfSpace(l, 1)
+
+	valid := verifyHalfSpace(len(data), l, VO, tree.Digest, 1, tree.Fanout)
+
+	assert.True(valid, "Should be true")
+
+}
+
+func TestCountArea(t *testing.T) {
+	testStartLabel(t)
+	defer testEndLabel()
+
+	assert := assert.New(t)
+
+	data := [][2]float64{
+		{1, 2},
+		{3, 4},
+		{5, 6},
+		{7, 8},
+		{9, 10},
+		{11, 12},
+		{13, 14},
+		{15, 16},
+		{17, 18},
+		{19, 20},
+		{21, 22},
+		{23, 24},
+	}
+
+	tree, _ := NewRTree(data, 3, sumOfSlice, one)
+
+	area := [4]float64{5, 12, 11, 6}
+
+	VO := tree.AuthCountArea(area)
+
+	res, valid := AuthCountVerify(VO, tree.Digest, tree.Fanout)
+
+	assert.Equal(4, res, "Should be 4")
+	assert.True(valid, "Should be true")
+
+}
+
+func TestHalfspaceCountNegative(t *testing.T) {
+	testStartLabel(t)
+	defer testEndLabel()
+
+	assert := assert.New(t)
+
+	data := [][2]float64{
+		{1, 2},
+		{-3, -4},
+		{-5, -6},
+		{-4, -2},
+		{-5, -2},
+		{-35, -2},
+		{-5, -23},
+	}
+
+	tree, _ := NewRTree(data, 3, sumOfSlice, one)
+
+	l := new(line)
+
+	l.B = 0
+	l.M = 0
+
+	VO := tree.AuthCountHalfSpace(l, 1)
+
+	valid := verifyHalfSpace(len(data), l, VO, tree.Digest, 1, tree.Fanout)
+
+	assert.False(valid, "Should be false")
+
+}
 func TestHalfspaceCount(t *testing.T) {
 	testStartLabel(t)
 	defer testEndLabel()
